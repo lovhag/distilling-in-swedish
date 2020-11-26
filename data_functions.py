4579cd6e129336430708d73068017590aba28582
Lovisa Hagström <lovhagstrom@gmail.com>
2020-11-26 16:17:57 +0100

    Adapt SUC data formatter to CONLL format.

--- data_functions.py
+++ data_functions.py
@@ -5,21 +5,33 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 _DIALECT = {}
-_DIALECT['delimiter'] = ','
+_DIALECT['delimiter'] = '\t'
 _DIALECT['lineterminator'] = '\n'#'\r\n'
 _DIALECT['escapechar'] = '\\'
 csv.register_dialect('NER_data_format', quoting=csv.QUOTE_NONE, delimiter=_DIALECT['delimiter'], lineterminator=_DIALECT['lineterminator'], escapechar=_DIALECT['escapechar'])
 
 _SUC_DATA_LOCATION = 'data/suc3.xml'
-_NER_TEXT_FILENAME = 'NER_text.csv'
-_NER_ENTITY_TYPE_FILENAME = 'NER_entity_type.csv'
+_NER_FILENAME = 'NER.tsv'
+_NER_TEXT_FILENAME = 'NER_text.tsv'
+_NER_ENTITY_TYPE_FILENAME = 'NER_entity_type.tsv'
 _NO_ENTITY_TAG = 'O'
 _SAVE_TO_DATA_LOCATION = 'data/'
+
+SUC_to_CONLL = {'person': 'PER',
+                'animal': 'PER',
+                'myth': 'PER',
+                'place': 'LOC',
+                'inst': 'ORG',
+                'product': 'MISC',
+                'work': 'MISC',
+                'event': 'MISC',
+                'other': 'MISC'
+}
 
 class NameKeeper():
     def __init__(self):
         self.nbr_name_tags = 0
-        self.nbr_per_name_tag = {'person': 0, 
+        self.SUC_tags = {'person': 0, 
                                  'animal': 0, 
                                  'myth': 0, 
                                  'place': 0, 
@@ -29,44 +41,46 @@ class NameKeeper():
                                  'event': 0, 
                                  'other': 0
                                  }
+        self.CONLL_tags = {'LOC': 0,
+                           'MISC': 0,
+                           'ORG': 0,
+                           'PER': 0}
 
     def get_name_info(self, name_entity):
         # there can be more than one word after a name tag. e.g. Vytautas Landsbergis
         # there can also be ne _overlap within names, we only want the words
-        name_entity_words = []
-        name_entity_types = []
+        name_entity_rows = []
+        name_type = SUC_to_CONLL[name_entity.attrib['type']]
         for child in name_entity:
             if child.tag == "w":
-                name_entity_words.append(child.text)
-                name_entity_types.append(name_entity.attrib['type'])
+                name_entity_rows.append([child.text, name_type])
             elif child.tag == "ne":
                 for ne_child in child:
                     if ne_child.tag == "w":
-                        name_entity_words.append(ne_child.text)
-                        name_entity_types.append(name_entity.attrib['type'])
+                        name_entity_rows.append([ne_child.text, name_type])
                     else:
                         raise ValueError(ne_child.tag)
             else:
                 raise ValueError(child.tag)
             
-        if len(set(name_entity_types)) > 1:
-            # expect to only have one type per name tag
-            raise ValueError(name_entity_types)
-        
-        self.nbr_name_tags += len(name_entity_types)
-        self.nbr_per_name_tag[name_entity_types[0]] += len(name_entity_types)
+        self.nbr_name_tags += len(name_entity_rows)
+        self.SUC_tags[name_entity.attrib['type']] += len(name_entity_rows)
+        self.CONLL_tags[name_type] += len(name_entity_rows)
 
-        return name_entity_words, name_entity_types
+        return name_entity_rows
 
 def saveNERdataFromSUC():
+    # want entity type tags of LOC|MISC|ORG|PER
+    # sort of following format described by "Introduction to the CoNLL-2003 Shared Task:
+    # Language-Independent Named Entity Recognition"
+    
     tree = ET.parse(_SUC_DATA_LOCATION)
     root = tree.getroot()
 
     print(f"NER data save process started. Saving data to {_SAVE_TO_DATA_LOCATION}...")
 
-    with open (_SAVE_TO_DATA_LOCATION+_NER_TEXT_FILENAME, 'w') as f, open (_SAVE_TO_DATA_LOCATION+_NER_ENTITY_TYPE_FILENAME, 'w') as g:
-        text_writer = csv.writer(f, dialect='NER_data_format')
-        entity_type_writer = csv.writer(g, dialect='NER_data_format')
+    with open (_SAVE_TO_DATA_LOCATION+_NER_FILENAME, 'w') as f:
+        writer = csv.writer(f, dialect='NER_data_format')
 
         num_saved = 0
         nbr_extra_ne = 0
@@ -80,34 +94,33 @@ def saveNERdataFromSUC():
             sentence_entity_types = []
             for child in sentence:
                 if child.tag == "w":
-                    sentence_words.append(child.text)
-                    sentence_entity_types.append(_NO_ENTITY_TAG)
+                    writer.writerow([child.text, _NO_ENTITY_TAG])
                 elif child.tag == "ne":
                     for subchild in child:
                         if subchild.tag == "name":
-                            entity_text, entity_type = name_keeper.get_name_info(subchild)
-                            sentence_words.extend(entity_text)
-                            sentence_entity_types.extend(entity_type)
+                            entity_rows = name_keeper.get_name_info(subchild)
+                            writer.writerows(entity_rows)
                         elif subchild.tag == "w":
                             nbr_extra_ne += 1
+                            writer.writerow([subchild.text, _NO_ENTITY_TAG])
                         else:
                             # can a ne tag contain anything other than name or word?
                             raise ValueError(sentence_index)
                 elif child.tag == "name":
                     nbr_extra_name += 1
-                    entity_text, entity_type = name_keeper.get_name_info(child)
-                    sentence_words.extend(entity_text)
-                    sentence_entity_types.extend(entity_type)
+                    entity_rows = name_keeper.get_name_info(child)
+                    writer.writerows(entity_rows)
                 else:
                     raise ValueError(child.tag)
+            writer.writerow([])
             num_saved += 1
-            text_writer.writerow(sentence_words)
-            entity_type_writer.writerow(sentence_entity_types)
 
     print(f"NER data saved! \n{sentence_index} sentences available. \nSaved a total of {num_saved} sentences.")
     print(f"The dataset contains a total of {name_keeper.nbr_name_tags} named entity tokens.")
-    print(f"Number of named entity tokens per classification category:")
-    [print(f"{category}: {number_of_tokens}") for category, number_of_tokens in name_keeper.nbr_per_name_tag.items()]
+    print(f"Number of SUC named entity tokens per classification category:")
+    [print(f"{category}: {number_of_tokens}") for category, number_of_tokens in name_keeper.SUC_tags.items()]
+    print(f"Number of CONLL named entity tokens per classification category:")
+    [print(f"{category}: {number_of_tokens}") for category, number_of_tokens in name_keeper.CONLL_tags.items()]
     print(f"There were {nbr_extra_name} name tags not covered by ne tags and {nbr_extra_ne} ne tags not covered by name tags.")
 
 def save_splits_from_file(read_filename, write_train_filename, write_eval_filename, write_test_filename, train_ix, eval_ix, test_ix):
@@ -160,5 +173,5 @@ def create_NER_datasets():
 
     # save to files
 
-#saveNERdataFromSUC()
-create_splits_from_saved_NER_data(74245)
+saveNERdataFromSUC()
+#create_splits_from_saved_NER_data(74245)
