9d658916b2419294f565554a8f979be6e88af2a7
Lovisa Hagstrom <lovhagstrom@gmail.com>
2020-12-03 07:30:03 +0100

    Add working code for running NER on titan.

---
+++ train_on_NER_old_titan.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/home/lovhag/miniconda3/envs/distilling-in-swedish-run-ner/bin/python /home/lovhag/projects/transformers/examples/token-classification/run_ner_old.py /home/lovhag/projects/distilling-in-swedish/run_ner_arguments_titan.json >> /home/lovhag/projects/distilling-in-swedish/run_ner_log.log
